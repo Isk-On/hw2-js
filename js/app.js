14a8a43d43095807306a470d947bcf59625a7e0a
Isk-On <119677180+Isk-On@users.noreply.github.com>
2023-02-01 16:20:24 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const parol = prompt("Логин", "");
+
+if (parol == "админ") {
+  let admin = prompt("Введите пароль");
+
+  if (admin == 12345) {
+    alert("e salom ozbelaga");
+  } else {
+    alert("fu");
+}}
+
+else {
+  alert("admin mas san");
+}
